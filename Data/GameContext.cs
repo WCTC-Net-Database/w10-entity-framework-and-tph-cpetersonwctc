b360b373b1c4df3891f553d1b1501cc884da764f
cpetersonwctc <cpeterson49@my.wctc.edu>
2024-11-06 16:38:41 -0600

    uuhmmm im not sure

--- Data/GameContext.cs
+++ Data/GameContext.cs
@@ -8,6 +8,7 @@ public class GameContext : DbContext
 {
     public DbSet<Room> Rooms { get; set; }
     public DbSet<Character> Characters { get; set; }
+    public DbSet<Ability> Abilities { get; set; }
 
     public GameContext(DbContextOptions<GameContext> options) : base(options)
     {
@@ -21,6 +22,22 @@ public class GameContext : DbContext
             .HasValue<Player>("Player")
             .HasValue<Goblin>("Goblin");
 
+        modelBuilder.Entity<Ability>()
+            .HasDiscriminator<string>("Discriminator")
+            .HasValue<PlayerAbility>("Player")
+            .HasValue<GoblinAbility>("Goblin");
+
+        modelBuilder.Entity<Character>()
+            .HasMany(c => c.Abilities)
+            .WithMany(a => a.Characters)
+            .UsingEntity(j => j.ToTable("CharacterAbilities"));
+
         base.OnModelCreating(modelBuilder);
     }
 }
+
+
+//dotnet ef database update
+//dotnet ef migrations add InitialCreate
+//dotnet ef database update 0
+//dotnet ef migrations remove
---
+++ Migrations/20241106220657_SeedAbilities.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace W9_assignment_template.Migrations
+{
+    public partial class SeedAbilities : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Enable identity insert for Rooms
+            migrationBuilder.Sql("SET IDENTITY_INSERT Abilities ON");
+
+            // Insert Rooms
+            migrationBuilder.Sql("INSERT INTO Abilities (Id, Name, Description, Discriminator, Taunt) VALUES (1, 'Flashbang', 'Pull the pin and throw. It will blind all the enemies','Player', 1)");
+            migrationBuilder.Sql("INSERT INTO Abilities (Id, Name, Description, Discriminator, Shove) VALUES (2, 'Explode', 'Literally just explodes the enemy','Player', 1)");
+            migrationBuilder.Sql("INSERT INTO Abilities (Id, Name, Description, Discriminator, Taunt) VALUES (3, 'Yell', 'A yell to gain attention','Goblin', 1)");
+            // Disable identity insert for Rooms
+            migrationBuilder.Sql("SET IDENTITY_INSERT Abilities OFF");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
